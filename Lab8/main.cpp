05114904f80cfa4db6d2f781e1f013c2a2ef34df
Cameron Brown <cameronjoebrown@gmail.com>
2018-12-10 16:24:21 -0700

    Initial Commit

---
+++ Lab8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab8
+//
+//  Created by Cam on 12/10/18.
+//  Copyright © 2018 Cam Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
