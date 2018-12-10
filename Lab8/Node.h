1e36f7396e2b2c2a778dbf30fc643bf8a7d23cad
Cameron Brown <cameronjoebrown@gmail.com>
2018-12-10 16:42:03 -0700

    First Commit

---
+++ Lab8/Node.h
@@ -0,0 +1,41 @@
+//
+//  Node.h
+//  Lab7
+//
+//  Created by Cam on 11/25/18.
+//  Copyright © 2018 Cameron Brown. All rights reserved.
+//
+
+#ifndef Node_h
+#define Node_h
+
+#include <stdio.h>
+#include "NodeInterface.h"
+
+class Node : public NodeInterface {
+public:
+    Node(int value, Node* L = nullptr, Node* R = nullptr) {
+        data = value;
+        leftChild = L;
+        rightChild = R;
+    }
+    ~Node() {
+        
+    }
+    
+    int getData() const {
+        return data;
+    }
+    NodeInterface* getLeftChild() const {
+        return leftChild;
+    }
+    NodeInterface* getRightChild() const {
+        return rightChild;
+    }
+    
+    Node* leftChild;
+    Node* rightChild;
+    int data;
+};
+
+#endif /* Node_h */
